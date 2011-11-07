40d291fcd85298e42b360f5d79afc6eacf11e2e5
termie <github@anarkystic.com>
2011-11-07 13:35:08 -0800

    add stubs for fetching data from jenkins

---
+++ stache/remote.py
@@ -0,0 +1,15 @@
+
+
+
+def fetch_latest(arch_ref, version_id=None, run_id=None):
+  """Fetch the latest results from jenkin for a given jenkins job.
+
+  Most likely run as a polling loop to import data for our displays.
+
+  """
+  pass
+
+
+def spawn_build(arch_ref, version_id=None, run_id=None):
+  """Trigger a build in jenkins and start a polling loop for the results."""
+  pass
